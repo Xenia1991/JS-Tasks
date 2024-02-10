e854aae3515a78208cf6cabce259a0173eb9ee58
Xenia <andreeva19-91@mail.ru>
2024-02-10 21:47:35 +0700

    add task 1.3.6

---
+++ 1.3.6/1.3.6.js
@@ -0,0 +1,23 @@
+function fizzbuzz(number) {
+	if (number%3 === 0 && number%5 === 0) {
+        return 'FizzBuzz';
+    } else if (number%3 === 0) {
+        return 'Fizz';
+    } else if (number%5 === 0) {
+        return 'Buzz'
+    } else {
+        return number;
+    }
+}
+
+console.log (fizzbuzz(45));
+console.log (fizzbuzz(25));
+console.log (fizzbuzz(15));
+console.log (fizzbuzz(9));
+console.log (fizzbuzz(8));
+/* 
+Напишите функцию, которая принимает число.
+Если число кратно 3, функция должна возвращать строку «Fizz».
+Если кратно 5, функция должна возвращать строку «Buzz».
+Если кратно и 3 и 5 - строку «FizzBuzz». В остальных случаях функция должна вернуть переданное число.
+*/
