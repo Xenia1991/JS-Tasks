078ac0a49c018d63a0102d36c687fe26703c3491
Xenia <andreeva19-91@mail.ru>
2024-02-16 17:46:15 +0700

    new task

--- 1.3.9/1.3.9.js
+++ 1.3.9/1.3.9.js
@@ -1,4 +1,31 @@
+const areBracketsBalanced = (str) => {
+    let bracketStack = [];
+    if (str.length === 0) {
+        return true;
+    } else if (str[0] === ')') {
+        return false;
+    }
+    for (let i=0; i<str.length; i++) {
+        if (bracketStack.length === 0 && str[i] === '(') {
+            bracketStack.push(str[i])
+        } else if (str[i] === bracketStack[bracketStack.length-1] && str[i] === '(') {
+            bracketStack.push(str[i]); 
+        } else if (bracketStack.length === 0 && str[i] === ')') {
+            return false;
+        } else {
+            bracketStack.pop(bracketStack[bracketStack.length-1]); 
+        }
+    }
+    return bracketStack.length === 0;
+};
 
+console.log(areBracketsBalanced(''));
+console.log(areBracketsBalanced('())('));
+console.log(areBracketsBalanced('()()()()()()()()'));
+console.log(areBracketsBalanced('()()()())))'));
+console.log(areBracketsBalanced('()()(()'));
+console.log(areBracketsBalanced('()()()'));
+console.log(areBracketsBalanced('(())())'));
 
 
 /* Реализуйте функцию, которая принимает на вход строку, состоящую только 
