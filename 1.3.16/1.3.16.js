04575956cb9f40b830230ef7a2250c4b18f48722
Xenia <andreeva19-91@mail.ru>
2024-02-17 21:13:58 +0700

    add new task 1.3.16

---
+++ 1.3.16/1.3.16.js
@@ -0,0 +1,26 @@
+const hasArrays = (arr) => {
+  for (const element of arr) {
+    if (Array.isArray(element)) {
+        return true;
+    }
+  }
+  return false;
+}
+
+const data1 = [false, true, [1, 2], {}, [], 1, 0, NaN];
+console.log(hasArrays(data1)); 
+// true
+
+const data2 = [];
+console.log(hasArrays(data2)); 
+// false
+
+const data3 = [[]];
+console.log(hasArrays(data3)); 
+// true
+
+
+
+/*Реализуйте функцию hasArrays, которая принимает массив данных разных типов 
+и возвращает true, если этот массив содержит массив внутри себя. Если нет, 
+функция должна вернуть false.*/
