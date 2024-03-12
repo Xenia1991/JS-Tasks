bad18d57ded5d442eaa9e308107d08423d4fd986
Xenia <andreeva19-91@mail.ru>
2024-03-12 18:10:45 +0300

    add task 2.2.3

--- 2.2.3/2.2.3.js
+++ 2.2.3/2.2.3.js
@@ -1,34 +1,40 @@
 function deepEqual (obj1, obj2) {
-    if (obj1 === null || obj2 === null) {
-        return false;
-    } else if (typeof(obj1) === 'object' && typeof(obj2) === 'object') {
-        
-        let key1 = Object.keys(obj1);
-        let key2 = Object.keys(obj2);
-        let value1 = Object.values(obj1);
-        let value2 = Object.values(obj2); 
-
-        if (key1.length === key2.length) {
-            for (let key in key1) {
-                for (let value in value1)
-                    if (typeof(value1[value]) !=='object' && typeof(value2[value])!=='object' ||
-                        value1[value] === null && value2[value] === null
-                    ) {
-                        console.log('Я туть');
-                        return value1[value] === value2[value] ? true : false;
-                    } else if (typeof(value1[value]) ==='object' && typeof(value2[value])==='object' ) {
-                        console.log('Я в рекурсии');
-                        return deepEqual(value1[value], value2[value]);
-                }
-            }
-        } else {
-            return false;
-        }
-
-    } else {
-        return obj1 === obj2 ? true : false;
+    if (typeof(obj1) !== 'object' && typeof(obj2) !== 'object') {
+        return obj1===obj2 ? true : false;
     }
+
+    if (obj1 === null && obj2 === null) {
+        return true;
+    } else if (obj1 === null || obj2 === null) {
+        return false;
+    }
+
+    const keys1 = Object.keys(obj1);
+    const keys2 = Object.keys(obj2);
+
+    if (keys1.length !== keys2.length) {
+    return false;
+    }
+
+    for (const key of keys1) {
+    const val1 = obj1[key];
+    const val2 = obj2[key];
+    const areObjects = isObject(val1) && isObject(val2);
+    if (
+        areObjects && !(val1, val2) ||
+        !areObjects && val1 !== val2
+    ) {
+        return false;
+    }
+    }
+
+    return true;
 }
+
+function isObject(object) {
+  return object != null && typeof object === 'object';
+}
+
 
 const firstObject = {
     a: {
@@ -41,7 +47,7 @@ const firstObject = {
          }
     }
 };
-  
+
 const secondObject = {
     a: {
       b: {
@@ -56,11 +62,13 @@ const secondObject = {
   
 console.log(deepEqual(firstObject, secondObject)); // true
 console.log(deepEqual({ a:1, b: 3 }, { b: 2, a: 1})); // false
+console.log(deepEqual({ a:1, b: 3 }, { b: 2, a: 1, c: 6})); // false
 console.log(deepEqual(1, 2)); //false
 console.log(deepEqual(true, false)); //false
+console.log(deepEqual(null, null)); //true
+console.log(deepEqual(5, null)); //false
 
-/* Напишите функцию, которая проверяет на равенство два объекта, учитывая их вложенность.
-
+/*
 Два объекта считаются равными, если у них все свойства одинаковы. 
 В случае, если одно из свойств - само объект, мы сравниваем на равенство эти объекты по тому же алгоритму.*/
 
