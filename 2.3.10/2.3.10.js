0fdca04934d31064a181328a2869c8457b4388f4
Xenia <andreeva19-91@mail.ru>
2024-03-27 18:21:35 +0300

    add new task 2.3.10

--- 2.3.10/2.3.10.js
+++ 2.3.10/2.3.10.js
@@ -10,37 +10,69 @@ class Person {
     }
 
     getAge() {
-        return new Date().getFullYear() - new Date(this.birthDate).getFullYear()
+        const userBirthDay = new Date(this.birthDate).getDay();
+        const currentDay = new Date().getDay();
+        let userAge = new Date().getFullYear() - new Date(this.birthDate).getFullYear();
+        if (userBirthDay > currentDay) {
+            return userAge-1
+        } else {
+            return userAge
+        }
     }
-
 }
-
-const person = new Person('Johannes', 'Helms', '1983-01-02');
-console.log(person);
-console.log(person.fullName);
-console.log(person.getAge());
 
 class Account {
     constructor(person, balance) {
         this.person = person
         this.balance = balance
-        this.description = null
         this.operations = []
-        this.amount = null
+    }
+
+    addOperation(timestamp, target, amount, description) {
+        const addOperation = {
+            timestamp,
+            target,
+            amount,
+            description,
+        }
+        this.operations.push(addOperation);
     }
 
     addMoney(amount, description) {
-        this.description = description;
-        this.amount = amount;
-        this.balance = this.balance + this.amount;
-        return this.amount, this.description;
+        this.addOperation(new Date().getTime(), 'in', amount, description);
+        this.balance = this.balance + amount;
+        return amount, description
     }
 
+
     withdrawMoney(amount, description) {
-        this.description = description
-        this.amount = amount;
-        this.balance = this.balance - this.amount;
-        return this.amount, this.description
+        this.addOperation(new Date().getTime(), 'out', amount, description);
+        this.balance = this.balance - amount;
+        return amount, description
+    }
+    
+    static transfer(fromAccount, toAccount, amount) {
+        if (fromAccount) {
+            const addOperation = {
+                timestamp: new Date().getTime(),
+                target: 'out',
+                amount: amount,
+                description: 'Перевод между счетами',
+            }
+            fromAccount.operations.push(addOperation)
+        }
+        if (toAccount) {
+            const addOperation = {
+                timestamp: new Date().getTime(),
+                target: 'in',
+                amount: amount,
+                description: 'Перевод между счетами',
+            }
+            toAccount.operations.push(addOperation)
+        }
+        fromAccount.balance = fromAccount.balance - amount;
+        toAccount.balance = toAccount.balance + amount;
+        return amount;
     }
 
     getAmount() {
@@ -48,69 +80,27 @@ class Account {
     }
 
     getAccountHistory() {
-        if (Account.prototype.addMoney(this.amount, this.description) || Account.prototype.withdrawMoney(this.amount, this.description)) {
-            let addOperation = {
-                timestamp: new Date().getTime(),
-                target:Account.prototype.addMoney() ? 'in' : 'out',
-                amount: this.amount,
-                description: this.description,
-            }
-            this.operations.push(addOperation);
-        }
-        // if(Account.prototype.withdrawMoney) {
-        //     let operation = {
-        //         timestamp: new Date().getTime(),
-        //         target: 'out',
-        //         amount: this.amount,
-        //         description: this.description,
-        //     }
-        //     this.operations.push(operation);
-        // }
-        return this.operations
-    }
-
-    static transfer(fromAccount, toAccount, amount) {
-        fromAccount.balance = fromAccount.balance - amount;
-        toAccount.balance = toAccount.balance + amount;
-        return amount;
+        return this.operations;
     }
 }
 
 const alex = new Person('Alexey', 'Petrov', '1994-05-22');
 const alexAccount = new Account(alex, 1000);
-console.log(alexAccount);
+console.log(alex.getAge());
 const helen = new Person('Helen', 'Smith', '1990-06-06');
 const helenAccount = new Account(helen, 400);
+console.log(helen.getAge());
 
 console.log(alexAccount.addMoney(1000, 'Зарплата'));
 const amount = alexAccount.getAmount();
-console.log(amount);
 console.log(alexAccount.withdrawMoney(amount * 0.1, 'Налоги'));
 console.log(Account.transfer(alexAccount, helenAccount, 100));
 console.log(helenAccount.getAmount());// 500
 console.log(alexAccount.getAmount());// 1700
 console.log(alexAccount.getAccountHistory());
+console.log(helenAccount.getAccountHistory());
+
 /* Личный Счет
-
-Account
-new Account(person, 1000);
-
-Методы
-addMoney(amount, description) - Положить деньги на аккаунт с комментарием 
-к переводу
-withdrawMoney(amount, description) - Вывести деньги с аккаунта с комментарием
-к переводу
-getAmount() - Получить текущее состояние счета
-getAccountHistory() - Возвращает массив с объектами формата 
-{ timestamp: 1574434091131, target: 'in', amount: 10, description: 'ЗП' }. 
-Поле target может иметь значения in или out.
-transfer(fromAccount, toAccount, amount) - статический метод, переводит 
-деньги с одного счета на другой
-
-Свойства
-
-person - Владелец счета
-
 Создайте 2 класса:
 Person для описания клиента и 
 Account для работы с банковским счетом частного лица.
@@ -129,16 +119,21 @@ firstName - Имя
 lastName - Фамилия
 fullName - Имя вместе с фамилией, вычислямое свойство (используем геттер)
 
+Account
+new Account(person, 1000);
 
+Методы
+addMoney(amount, description) - Положить деньги на аккаунт с комментарием 
+к переводу
+withdrawMoney(amount, description) - Вывести деньги с аккаунта с комментарием
+к переводу
+getAmount() - Получить текущее состояние счета
+getAccountHistory() - Возвращает массив с объектами формата 
+{ timestamp: 1574434091131, target: 'in', amount: 10, description: 'ЗП' }. 
+Поле target может иметь значения in или out.
+transfer(fromAccount, toAccount, amount) - статический метод, переводит 
+деньги с одного счета на другой
 
+Свойства
 
-
-
-
-
-
-
-
-
-
-*/
+person - Владелец счета */
